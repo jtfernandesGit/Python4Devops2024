188e74aef86f1ea0afc4198984998156a749b25f
jtfernandes <28363974+jtfernandesGit@users.noreply.github.com>
2024-04-16 19:36:33 +0000

    update test coverage

--- test_main.py
+++ test_main.py
@@ -3,8 +3,20 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from mylib.calculator import add, subtract, multiply, divide
 
 
 def test_add():
     assert add(1, 2) == 3
+
+
+def test_subtract():
+    assert subtract(7, 3) == 4
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+
+def test_division():
+    assert divide(6, 3) == 2
